5b87bcd37f10b66f8932e6671a266fd83f65dc0c
arya002 <54712381+arya002@users.noreply.github.com>
2020-07-10 04:25:40 +0800

    repository initialization

---
+++ precache-manifest.b83aaf8c450d8eabf29c7b26ce8160ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec966fe247e695f3e1748be83c49ce18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9dffb316639e79fa48b",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2ddfd92370ba54741204",
+    "url": "./static/css/main.193ef9e0.chunk.css"
+  },
+  {
+    "revision": "d9dffb316639e79fa48b",
+    "url": "./static/js/2.9db25aab.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.9db25aab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ddfd92370ba54741204",
+    "url": "./static/js/main.7194a7c7.chunk.js"
+  },
+  {
+    "revision": "02e5aaf2d7372088ea50",
+    "url": "./static/js/runtime-main.47f1b240.js"
+  }
+]);
